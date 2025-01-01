457cb693c89ad8bf03047ea4aab2607725d6c4fc
JefersonPMS <jefersoonsb09@hotmail.com>
2025-01-01 17:39:07 -0300

    Adiciona pipeline de build e deploy no Cloud Run3

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("Olá, Mundo!")
+    st.title("Olá, Mundo! ")
     st.write("Este é um app Streamlit rodando em Docker.")
 
 if __name__ == "__main__":
